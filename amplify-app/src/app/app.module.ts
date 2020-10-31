d5f107edbcb3914fbc587b020794b0d4d4dc5aff
Jagadeesh_Dachepalli <JDachepalli@air-worldwide.com>
2020-10-31 18:33:51 +0530

    Initial commit - Contains basic amplify angualr GraphQL app

---
+++ amplify-app/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+/* import AmplifyUIAngularModule  */
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+
+/* new form imports */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    /* configure app with AmplifyUIAngularModule */
+    AmplifyUIAngularModule,
+    /* configuring form modules */
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
