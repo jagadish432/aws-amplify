0ef5c9af83d46c2cf7004ab4abc9dab141e3e9ea
Jagadeesh_Dachepalli <JDachepalli@air-worldwide.com>
2020-11-03 16:35:21 +0530

    working API gateway route with lambda func

--- amplify-app/amplify/backend/function/postsLambda/src/index.py
+++ amplify-app/amplify/backend/function/postsLambda/src/index.py
@@ -1,7 +1,13 @@
+import json
+
 def handler(event, context):
   print('received event:')
   print(event)
   print("added one sample line for testing")
+  responseBody = {
+    "message": "hey from Lambda !!!"
+  }
   return {
-    'message': 'Hello from your new Amplify Python lambda!'
+    "statusCode": 200,
+    "body": json.dumps(responseBody)
   }
